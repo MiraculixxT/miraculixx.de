99758398f21b43cb84dda2a9ed3d3ea1d6207180
Miraculixx <miraculixxyt@gmail.com>
2025-01-13 15:18:35 +0100

    Auto detect production environment

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,3 @@
 
-export const PRODUCTION = false;
+export const PRODUCTION = process.env.NODE_ENV === 'production';
 export const API_URL = PRODUCTION ? "https://api.miraculixx.de" : "http://localhost:8080";
